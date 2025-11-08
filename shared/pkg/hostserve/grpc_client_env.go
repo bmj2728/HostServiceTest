338e42e89f67581b44e20be205e0b453325a371a
Brian Jipson <bmj2728@gmail.com>
2025-11-08 17:23:16 -0500

    Implement `GetEnv` in `HostServiceGRPCServer` and `HostServiceGRPCClient`
    - Added `GetEnv` method implementation in `HostServiceGRPCServer` to retrieve environment variables.
    - Completed `GetEnv` logic in `HostServiceGRPCClient` to query environment variables via gRPC call.

--- shared/pkg/hostserve/grpc_client_env.go
+++ shared/pkg/hostserve/grpc_client_env.go
@@ -1,6 +1,17 @@
 package hostserve
 
+import (
+	"context"
+
+	hostservev1 "github.com/bmj2728/hst/shared/protogen/hostserve/v1"
+)
+
 func (c *HostServiceGRPCClient) GetEnv(key string) string {
-	//TODO implement me
-	panic("implement me")
+	resp, err := c.client.GetEnv(context.Background(), &hostservev1.GetEnvRequest{
+		Key: key,
+	})
+	if err != nil {
+		return ""
+	}
+	return resp.Val
 }
--- shared/pkg/hostserve/grpc_server_env.go
+++ shared/pkg/hostserve/grpc_server_env.go
@@ -6,6 +6,7 @@ import (
 	hostservev1 "github.com/bmj2728/hst/shared/protogen/hostserve/v1"
 )
 
-func (s *HostServiceGRPCServer) GetEnv(context.Context, *hostservev1.GetEnvRequest) (*hostservev1.GetEnvResponse, error) {
-	panic("implement me")
+func (s *HostServiceGRPCServer) GetEnv(ctx context.Context, request *hostservev1.GetEnvRequest) (*hostservev1.GetEnvResponse, error) {
+	val := s.Impl.GetEnv(request.Key)
+	return &hostservev1.GetEnvResponse{Val: val}, nil
 }
